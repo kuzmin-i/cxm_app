bd3d01640f14e8ccb3a4301128335029bcf0057b
Ilya Kuzmin <68997547+kuzmin-i@users.noreply.github.com>
2022-04-30 16:37:07 +0300

    upd

--- pages/fetch/index.js
+++ pages/fetch/index.js
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 const FecthPage = () => {
   const fetchTodos = async () => {
     const response = await fetch(
-      "https://51.250.19.71:8080/mmodel_method/estimate_plane_collisions"
+      "http://51.250.19.71:8080/mmodel_method/estimate_plane_collisions"
     );
     const todos = await response.json();
 
